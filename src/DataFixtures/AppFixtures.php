9720c647f52697432d2b013d835998734807ed0f
Laurent <laurentleponner@gmail.com>
2022-05-31 11:10:58 +0200

    Création participant2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -90,6 +90,21 @@ class AppFixtures extends Fixture
             );
         $manager->persist($participant1);
 
+        $participant2 = new Participant();
+        $p2pwd = 'p2pwd';
+        $participant2->setName($faker->lastName())
+            ->setFirstname($faker->firstName())
+            ->setPhone($faker->phoneNumber())
+            ->setActive(true)
+            ->setEmail($faker->email())
+            ->setCampus($campus1)
+            ->setPassword(
+                $passwordHasher->hashPassword(
+                    $participant2,
+                    $p2pwd
+                )
+            );
+        $manager->persist($participant2);
 
         $manager->flush();
     }
